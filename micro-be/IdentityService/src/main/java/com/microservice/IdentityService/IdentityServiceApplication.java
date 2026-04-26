bf8fc39dd9f7ce78f44df5d23e37368fbf1525b2
myapah76 <140422521+myapah76@users.noreply.github.com>
2026-04-26 17:20:45 +0700

    Update/ Restructure files into 1 module

--- micro-be/IdentityService/src/main/java/com/microservice/IdentityService/IdentityServiceApplication.java
+++ micro-be/IdentityService/src/main/java/com/microservice/IdentityService/IdentityServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservice.IdentityService;
+package com.microservice.identityService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
